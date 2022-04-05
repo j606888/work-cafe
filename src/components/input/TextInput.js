89a03e435ad8a7676525280628bcf128721d003e
James <j606888@gmail.com>
2022-04-05 22:55:12 +0800

    Add RadioInput

--- src/components/input/TextInput.js
+++ src/components/input/TextInput.js
@@ -12,10 +12,11 @@ function TextInput({name, formik, type='text'}) {
       type={type}
       label={capitalizeFirstLetter(name)}
       fullWidth
-      value={formik.values[name]}
-      onChange={formik.handleChange}
-      error={formik.touched[name] && Boolean(formik.errors[name])}
-      helperText={formik.touched[name] && formik.errors[name]}
+      margin="dense"
+      value={formik && formik.values[name]}
+      onChange={formik && formik.handleChange}
+      error={formik && formik.touched[name] && Boolean(formik.errors[name])}
+      helperText={formik && formik.touched[name] && formik.errors[name]}
     />
   )
 }
