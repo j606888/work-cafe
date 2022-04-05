e8450a0aa527ddc0a799c5a43a4563981e9752d4
James <j606888@gmail.com>
2022-04-05 17:09:40 +0800

    Use formik to validate login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,6 +11,19 @@ import {
 } from "@material-ui/core"
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
 import { Link as RouterLink } from "react-router-dom"
+import { useFormik } from "formik"
+import * as yup from 'yup'
+
+const validationSchema = yup.object({
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string('Enter your password')
+    .min(6, 'Password must greater than 6')
+    .required('Password is required')
+})
 
 const Login = () => {
   const paperStyle = {
@@ -25,43 +38,71 @@ const Login = () => {
   const btnStyle = {
     margin: "8px 0",
   }
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    }
+  })
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
-        <Grid align="center">
-          <Avatar style={avatarStyle}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <h2>Log In</h2>
-        </Grid>
-        <TextField label="Email" placeholder="Enter email" fullWidth required />
-        <TextField
-          label="Password"
-          placeholder="Enter password"
-          type="password"
-          fullWidth
-          required
-        />
-        <FormControlLabel
-          control={<Checkbox name="checkedB" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          color="primary"
-          fullWidth
-          style={btnStyle}
-        >
-          Log in
-        </Button>
-        <Typography>
-          <Link href="#">Forgot password?</Link>
-        </Typography>
-        <Typography>
-          Don't have an account?{" "}
-          <Link component={RouterLink} to='/signup'>Sign up</Link>
-        </Typography>
+        <form onSubmit={formik.handleSubmit}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Log In</h2>
+          </Grid>
+          <TextField
+            id="email"
+            name="email"
+            label="Email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            fullWidth
+          />
+          <TextField
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            fullWidth
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            color="primary"
+            fullWidth
+            style={btnStyle}
+          >
+            Log in
+          </Button>
+          <Typography>
+            <Link href="#">Forgot password?</Link>
+          </Typography>
+          <Typography>
+            Don't have an account?{" "}
+            <Link component={RouterLink} to="/signup">
+              Sign up
+            </Link>
+          </Typography>
+        </form>
       </Paper>
     </Grid>
   )
