39735dec5370daa5f71b1e5cc56003aac083c066
James <j606888@gmail.com>
2022-04-09 19:14:21 +0800

    Split component

--- src/pages/GoogleMap.js
+++ src/pages/GoogleMap.js
@@ -1,5 +1,6 @@
 import { Wrapper } from "@googlemaps/react-wrapper"
-import { useRef, useEffect, useState } from "react"
+import Marker from "../components/google-map/Marker"
+import Map from "../components/google-map/Map"
 
 const GOOGLE_MAP_API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY
 
@@ -7,30 +8,15 @@ const render = (status) => {
   return <h1>{status}</h1>
 }
 
-function MyMapComponent({ center, zoom }) {
-  const ref = useRef()
-  const [map, setMap] = useState()
-
-  useEffect(() => {
-    
-    new window.google.maps.Map(ref.current, {
-      center,
-      zoom,
-    })
-  })
-
-  return <div ref={ref} style={{height: '500px'}}  />
-}
-
 const GoogleMap = () => {
   const center = { lat: 22.9956001, lng: 120.2101171 }
   const zoom = 16
-  console.log(GOOGLE_MAP_API_KEY)
-  console.log(process.env)
 
   return (
     <Wrapper apiKey={GOOGLE_MAP_API_KEY} render={render}>
-      <MyMapComponent center={center} zoom={zoom} />
+      <Map center={center} zoom={zoom}>
+        <Marker position={center} />
+      </Map>
     </Wrapper>
   )
 }
