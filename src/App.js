1e9d29b6d4efd7281fe197f8a76d78a596964e06
James <j606888@gmail.com>
2022-04-13 00:39:13 +0800

    Build new Login Page sample

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import Profile from "./pages/Profile"
-import HomePage from "./pages/HomePage"
-import GoogleMap from "./pages/GoogleMap"
-import SearchGoogleMapUrl from "./pages/SearchGoogleMapUrl"
+// import Signup from "./pages/Signup"
+import LoginPage from "./pages/LoginPage"
+// import Profile from "./pages/Profile"
+// import HomePage from "./pages/HomePage"
+// import GoogleMap from "./pages/GoogleMap"
+// import SearchGoogleMapUrl from "./pages/SearchGoogleMapUrl"
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        {/* <Route path="/" element={<HomePage />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<Login />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/google-map" element={<GoogleMap />}></Route>
-        <Route path="/search-google-map-url" element={<SearchGoogleMapUrl />}></Route>
+        <Route path="/search-google-map-url" element={<SearchGoogleMapUrl />}></Route> */}
       </Routes>
     </div>
   )
