a748cbf1d057c3fc0a9f80f8b5e9cb7027779a8d
James <j606888@gmail.com>
2022-04-17 16:37:13 +0800

    Update routing

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
+import ProfilePage from "./ProfilePage";
+import MapPage from "./MapPage";
+
+export { HomePage, LoginPage, SignupPage, ProfilePage, MapPage }
