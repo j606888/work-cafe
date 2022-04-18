8ed2ee7f62be48454081a2873c7f57c8d387ddc4
James <j606888@gmail.com>
2022-04-18 23:15:44 +0800

    Wrap MapComponent into a new componoent

---
+++ src/pages/MapPage/MapComponent.js
@@ -0,0 +1,21 @@
+import { useRef, useState } from "react"
+import useMarkers from "./useMarkers"
+import useGoogleMap from "./useGoogleMap"
+import StoreDrawer from "./StoreDrawer"
+
+function MapComponent() {
+  const [storeId, setStoreId] = useState(null)
+  const ref = useRef(null)
+  const map = useGoogleMap(ref)
+  useMarkers(map, setStoreId)
+
+  return (
+    <>
+      <StoreDrawer id={storeId} setStoreId={setStoreId} />
+      <div style={{ height: "10px" }} />
+      <div ref={ref} style={{ width: "100%", height: "80%" }}></div>
+    </>
+  )
+}
+
+export default MapComponent
--- src/pages/MapPage/index.js
+++ src/pages/MapPage/index.js
@@ -1,10 +1,7 @@
 import { Stack } from "@mui/material"
 import UserLayout from "../../components/layout/UserLayout"
 import { Wrapper } from "@googlemaps/react-wrapper"
-import { useRef, useState } from "react"
-import StoreDrawer from "./StoreDrawer"
-import useGoogleMap from "./useGoogleMap"
-import useMarkers from "./useMarkers"
+import MapComponent from "./MapComponent"
 
 const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY
 const style = {
@@ -12,19 +9,11 @@ const style = {
 }
 
 const MapPage = () => {
-  const [storeId, setStoreId] = useState(null)
-  const ref = useRef(null)
-  const map = useGoogleMap(ref)
-
-  useMarkers(map, setStoreId)
-
   return (
     <UserLayout>
       <Stack sx={style.stack}>
         <Wrapper apiKey={API_KEY}>
-          <StoreDrawer id={storeId} setStoreId={setStoreId} />
-          <div style={{ height: "10px" }} />
-          <div ref={ref} style={{ width: "100%", height: "80%" }}></div>
+          <MapComponent />
         </Wrapper>
       </Stack>
     </UserLayout>
