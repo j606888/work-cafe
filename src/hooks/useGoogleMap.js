ca76250adc01ecec97da8765983fda8891cb06e2
James <j606888@gmail.com>
2022-04-24 11:43:39 +0800

    Add location when moving

--- src/hooks/useGoogleMap.js
+++ src/hooks/useGoogleMap.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { createSearchParams, useSearchParams } from "react-router-dom"
 
 const DEFAULT_SETUP = {
   center: {
@@ -10,17 +11,31 @@ const DEFAULT_SETUP = {
 }
 
 export default function useGoogleMap({ ref, handleOnClick }) {
+  const [searchParams, setSearchParams] = useSearchParams()
   const [map, setMap] = useState(null)
 
   useEffect(() => {
     if (ref.current && !map) {
-      const googleMap = new window.google.maps.Map(ref.current, DEFAULT_SETUP)
+      const settings = DEFAULT_SETUP
+      const lat = searchParams.get('lat')
+      const lng = searchParams.get("lng")
+      if (lat && lng) {
+        settings.center = { lat: +lat, lng: +lng }
+      }
+      const googleMap = new window.google.maps.Map(ref.current, settings)
 
       googleMap.addListener("click", (mapMouseEvent) => {
         const location = mapMouseEvent.latLng.toJSON()
         if (handleOnClick) {
           handleOnClick(location.lat, location.lng)
         }
+      })
+
+      googleMap.addListener("dragend", () => {
+        const center = googleMap.getCenter()
+        const lat = center.lat().toFixed(6)
+        const lng = center.lng().toFixed(6)
+        setSearchParams(createSearchParams({ lat, lng }))
       })
 
       setMap(googleMap)
